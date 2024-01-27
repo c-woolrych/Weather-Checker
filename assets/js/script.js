184fd844faf9cff4c0bec73b3c55d5264107323a
c-woolrych <c.woolrych@gmail.com>
2024-01-27 13:40:22 +0000

    remove messy code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,23 @@
-// @returns {string} ??
+var city = $('#search-input').val().trim();
+var cityHistory = [];
 
-// function buildQueryURL() {
-//     var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}';
-//     var queryParams = { 'API-key': 'c602c0a5fb4280978dadc46ab1369f6a'};
-
-//     queryParams.q = $('#search-input').val().trim();
-
-// }
-var city // = $('#search-input').val().trim();
-var cities = [];
 
 $('#search-button').on('click', function(event) {
     event.preventDefault();
-    city = $('#search-input').val().trim();
-    renderButtons(city);
-    
+    renderButtons();
+    localStorage.setItem('cityBtn', JSON.stringify(cityHistory));
+    loadButtons();
 })
 
-function renderButtons(city) {
+function renderButtons() {
         var btn = $('<button>');
         btn.addClass('btn');
+        btn.addClass('btn-primary');
         btn.text(city);
         $('#history').append(btn);
-        localStorage.setItem('cities', JSON.stringify(cities));
+        localStorage.setItem('cityBtn', JSON.stringify(cityHistory));
 }
 console.log(cities);
 
-$('#search-input').each(function () {
-    cities.push($(this).val());
-});
 
-function loadButtons() {
-    const storedButtons = localStorage.getItem('cities');
-
-    if (storedButtons) {
-        const parsedButtons = JSON.parse(storedButtons);
-
-        parsedButtons.forEach((city) => {
-            var btn = $('<button>');
-            btn.addClass('btn');
-            btn.text(city);
-            $('#history').append(btn);
-        });
-    }
-}
 window.addEventListener('load', loadButtons);
