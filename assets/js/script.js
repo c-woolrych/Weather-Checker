b46bdd823ebb3508a82ec0228744df8459a72660
c-woolrych <c.woolrych@gmail.com>
2024-01-27 14:33:13 +0000

    re-write code to get buttons to persist

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,39 @@
-var city = $('#search-input').val().trim();
-var cityHistory = [];
+var cityHistory = $('#history');
+var cityArr = [];
 
 
+//event listener for search button
 $('#search-button').on('click', function(event) {
     event.preventDefault();
+    localStorage.setItem('city', (cityArr));
+
     renderButtons();
-    localStorage.setItem('cityBtn', JSON.stringify(cityHistory));
-    loadButtons();
+
 })
 
+// creates buttons of cities
 function renderButtons() {
+        var city = $('#search-input').val().trim();
         var btn = $('<button>');
+        btn.text(city);
         btn.addClass('btn');
         btn.addClass('btn-primary');
-        btn.text(city);
-        $('#history').append(btn);
-        localStorage.setItem('cityBtn', JSON.stringify(cityHistory));
+        cityHistory.append(btn);
+        cityArr.push(city);
+
 }
-console.log(cities);
+console.log(cityArr);
 
 
-window.addEventListener('load', loadButtons);
+window.addEventListener('load', function(event) {
+    event.preventDefault();
+    for (let i = 0; i < cityArr.length; i++) {
+        var btn = $('<button>');
+        // btn.text(city);
+        btn.attr('value', cityArr[i]);
+        btn.addClass('btn');
+        btn.addClass('btn-primary');
+        cityHistory.append(btn);
+    }
+    localStorage.getItem('cityHistory');
+});
