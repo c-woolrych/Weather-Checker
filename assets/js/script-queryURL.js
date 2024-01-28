61f66cd53b5fd8a05c553d993dc104db0f75378f
c-woolrych <c.woolrych@gmail.com>
2024-01-28 14:26:12 +0000

    build current weather data fetch

--- assets/js/script-queryURL.js
+++ assets/js/script-queryURL.js
@@ -9,6 +9,19 @@ $('#search-button').on('click', function() {
         })
         .then(function (data) {
             var results = results.data;
+
+            for (var i = 0; i < results.lengthl; i++) {
+                var weatherHrEl = $('.weather-hr');
+
+                var temp = results[i].main.temp.val();
+                var wind = results.wind.speed.val();
+                var humid = results.main.humidity.val();
+                var tempP = $('<p>').text('Temperature: ' + temp + 'C');
+                var windP = $('<p>').text('Wind: ' + wind + 'KPH');
+                var humidP = $('<p>').text('Humidity: ' + humid + '%');
+
+
+            }
         });
     })
 
