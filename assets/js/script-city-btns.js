1238d20e3e9f5ae5d10060a5db9d8876fba5e789
c-woolrych <c.woolrych@gmail.com>
2024-01-30 18:27:24 +0000

    continue working on fixing city buttons

--- assets/js/script-city-btns.js
+++ assets/js/script-city-btns.js
@@ -3,11 +3,12 @@ var searchBtnEl = $('#search-button');
 var cityInput = $('#search-input')
 var cityArr = [];
 
+var cityName = cityInput.val().trim();
+cityArr.push(cityName);
+
 //event listener for search button
 searchBtnEl.on('click', function(event) {
     event.preventDefault();
-    var city = cityInput.val().trim();
-    cityArr.push(city);
     localStorage.setItem('stored', (cityArr));
     renderButtons();
 })
@@ -16,19 +17,23 @@ var stored = localStorage.getItem('stored', (cityArr));
 function getStored() {
 
     for (let i = 0; i < stored.length; i++) {
-        var cityBtn = $('<button>').attr('class', 'list-group-item').text(stored);
+        var cityBtn = $('<button>').addClass('list-group-item btn m-1').text(stored).attr('style', 'background: lightgray');
         cityHistory.append(cityBtn);
         console.log(stored);
+
+cityBtn.on('click', function() {
+    currentWeather();
+    forecast();
+})
     }
 }
 getStored()
+
 // creates buttons of cities
 function renderButtons() {
-        var city = $('#search-input').val().trim();
-        var btn = $('<button>');
-        btn.text(city);
-        btn.addClass('btn');
-        btn.addClass('btn-primary');
-        cityHistory.append(btn);
-
+    var city = $('#search-input').val().trim();
+    var btn = $('<button>');
+    btn.text(city);
+    btn.addClass('btn');
+    cityHistory.append(btn);
 }
--- assets/js/script-queryURL.js
+++ assets/js/script-queryURL.js
@@ -9,14 +9,12 @@
 //     event.preventDefault();
 //     todayDate.clear();
 // });
+var cityName = cityInput.val().trim();
 
-
-// click event to fetch weather data of city searched
-$('#search-button').on('click', function(event) {
-    var cityName = $('#search-input').val().trim();
-    var queryURLcurrent = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=metric&appid=c602c0a5fb4280978dadc46ab1369f6a';
-    
     //query for current weather
+    function currentWeather() {
+        var cityName = $('#search-input').val().trim();
+        var queryURLcurrent = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=metric&appid=c602c0a5fb4280978dadc46ab1369f6a';
     fetch(queryURLcurrent)
     .then(function (response) {
             return response.json();
@@ -46,42 +44,52 @@ $('#search-button').on('click', function(event) {
                 cardTextEl.append(windP);
                 cardTextEl.append(humidP);
         });
+    };
 
-        //query for 5 day forecast
-    var queryURLforecast = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=c602c0a5fb4280978dadc46ab1369f6a&units=metric'
+            //query for 5 day forecast
+            function forecast() {
+        var cityName = $('#search-input').val().trim();
+                var queryURLforecast = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=c602c0a5fb4280978dadc46ab1369f6a&units=metric'
+            
+                fetch(queryURLforecast)
+                    .then(function (response) {
+                        return response.json();
+                    })
+                    .then(function(data) {
+                        
+                        for (var i = 0; i < data.list.length -1; i++) {
+                            var time = data.list[i].dt_txt;
+                            var forecastSection = $('#forecast');
+                            var dayDiv = $('#day');
+                            forecastSection.append(dayDiv);
+            
+                            if (time.includes('09:00:00')) {
+                                var iconURL = $('<img>').attr('src', 'https://openweathermap.org/img/wn/' + data.list[i].weather[0].icon + '@2x.png');
+                                // console.log(data.list[i].weather[0].icon);
+                                var date = data.list[i].dt_txt.slice(0, 10);
+                                var temp = data.list[i].main.temp;
+                                var wind = data.list[i].wind.speed;
+                                var humid = data.list[i].main.humidity;
+                                var dateP = $('<h5>').text(date).addClass('fw-bold');
+                                var tempP = $('<p>').text('Temperature: ' + temp + ' C');
+                                var windP = $('<p>').text('Wind Speed: ' + wind + ' MPH');
+                                var humidP = $('<p>').text('Humidity: ' + humid + '%');
+                                
+                                dayDiv.append(dateP)
+                                .append(iconURL)
+                                .append(tempP).append(windP).append(humidP);
+                            }
+                        }
+                        
+                        
+                    })
+                };
 
-    fetch(queryURLforecast)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function(data) {
-            
-            for (var i = 0; i < data.list.length -1; i++) {
-                var time = data.list[i].dt_txt;
-                var forecastSection = $('#forecast');
-                var dayDiv = $('#day');
-                forecastSection.append(dayDiv);
+// click event to fetch weather data of city searched
+searchBtnEl.on('click', function(event) {
+currentWeather();
+forecast()
+});
 
-                if (time.includes('09:00:00')) {
-                    var iconURL = $('<img>').attr('src', 'https://openweathermap.org/img/wn/' + data.list[i].weather[0].icon + '@2x.png');
-                    // console.log(data.list[i].weather[0].icon);
-                    var date = data.list[i].dt_txt.slice(0, 10);
-                    var temp = data.list[i].main.temp;
-                    var wind = data.list[i].wind.speed;
-                    var humid = data.list[i].main.humidity;
-                    var dateP = $('<h5>').text(date).addClass('fw-bold');
-                    var tempP = $('<p>').text('Temperature: ' + temp + ' C');
-                    var windP = $('<p>').text('Wind Speed: ' + wind + ' MPH');
-                    var humidP = $('<p>').text('Humidity: ' + humid + '%');
-                    
-                    dayDiv.append(dateP)
-                    .append(iconURL)
-                    .append(tempP).append(windP).append(humidP);
-                }
-            }
-            
-            
-        })
-})
 
 
